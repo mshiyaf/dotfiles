ee605f7ebe9c90cbc84f2453974b9e6a0fd89460
mshiyaf <mshiyaf@gmail.com>
2021-12-02 17:19:27 +0530

    added new lualine config with git, lsp, filename

--- nvim/.config/nvim/after/plugin/lspconfig.rc.vim
+++ nvim/.config/nvim/after/plugin/lspconfig.rc.vim
@@ -2,69 +2,67 @@ lua << EOF
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 require'lspconfig'.clangd.setup {
-    on_attach = on_attach,
-    root_dir = function() return vim.loop.cwd() end,
-    capabilities = capabilities
-}
+  on_attach = on_attach,
+  root_dir = function() return vim.loop.cwd() end,
+  capabilities = capabilities
+  }
 
 require'lspconfig'.gopls.setup{
-    on_attach=on_attach,
-    cmd = {"gopls", "serve"},
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-            },
-            staticcheck = true,
-        },
+on_attach=on_attach,
+cmd = {"gopls", "serve"},
+settings = {
+  gopls = {
+    analyses = {
+      unusedparams = true,
+      },
+    staticcheck = true,
     },
-    capabilities = capabilities
+  },
+capabilities = capabilities
 }
 
 require'lspconfig'.svelte.setup{}
 
 require'lspconfig'.yamlls.setup{}
 
-require'lspconfig'.tailwindcss.setup{}
-
 require'lspconfig'.pylsp.setup{}
 
 require'lspconfig'.tsserver.setup {
   on_attach = on_attach,
   capabilities = capabilities
-}
+  }
 
 
 -- icon
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    underline = true,
-    -- This sets the spacing and the prefix, obviously.
-    virtual_text = {
-      spacing = 4,
-      prefix = ''
+vim.lsp.diagnostic.on_publish_diagnostics, {
+  underline = true,
+  -- This sets the spacing and the prefix, obviously.
+  virtual_text = {
+    spacing = 4,
+    prefix = ''
     }
   }
 )
 
 local snippets_paths = function()
-    local plugins = { "friendly-snippets" }
-    local paths = {}
-    local path
-    local root_path = vim.env.HOME .. '/.vim/plugged/'
-    for _, plug in ipairs(plugins) do
-        path = root_path .. plug
-        if vim.fn.isdirectory(path) ~= 0 then
-            table.insert(paths, path)
-        end
-    end
-    return paths
+local plugins = { "friendly-snippets" }
+local paths = {}
+local path
+local root_path = vim.env.HOME .. '/.vim/plugged/'
+for _, plug in ipairs(plugins) do
+  path = root_path .. plug
+  if vim.fn.isdirectory(path) ~= 0 then
+    table.insert(paths, path)
+  end
+end
+return paths
 end
 
 require("luasnip.loaders.from_vscode").lazy_load({
-    paths = snippets_paths(),
-    include = nil,  -- Load all languages
-    exclude = {}
+paths = snippets_paths(),
+include = nil,  -- Load all languages
+exclude = {}
 })
 
 
--- nvim/.config/nvim/after/plugin/lualine.rc.lua
+++ nvim/.config/nvim/after/plugin/lualine.rc.lua
@@ -1,35 +1,158 @@
-local status, lualine = pcall(require, "lualine")
-if (not status) then return end
+local lualine = require "lualine"
 
-lualine.setup {
-  options = {
-    icons_enabled = true,
-    theme = 'gruvbox',
-    section_separators = {'', ''},
-    component_separators = {'', ''},
-    disabled_filetypes = {}
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {
-      { 'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '} },
-      'encoding',
-      'filetype'
-    },
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  extensions = {'fugitive'}
+local colors = {
+    bg = "#202328",
+    fg = "#bbc2cf",
+    yellow = "#ECBE7B",
+    cyan = "#008080",
+    green = "#98be65",
+    orange = "#FF8800",
+    violet = "#a9a1e1",
+    magenta = "#c678dd",
+    red = "#ec5f67"
 }
 
+local conditions = {
+    buffer_not_empty = function()
+        return vim.fn.empty(vim.fn.expand "%:t") ~= 1
+    end,
+    hide_in_width = function()
+        return vim.fn.winwidth(0) > 80
+    end,
+    check_git_workspace = function()
+        local filepath = vim.fn.expand "%:p:h"
+        local gitdir = vim.fn.finddir(".git", filepath .. ";")
+        return gitdir and #gitdir > 0 and #gitdir < #filepath
+    end
+}
+
+-- Config
+local config = {
+    options = {
+        -- Disable sections and component separators
+        icons_enabled = true,
+        component_separators = "",
+        section_separators = "",
+        theme = "gruvbox"
+    },
+    sections = {
+        -- these are to remove the defaults
+        lualine_a = {"mode"},
+        lualine_b = {},
+        lualine_y = {"progress"},
+        lualine_z = {"location"},
+        -- These will be filled later
+        lualine_c = {},
+        lualine_x = {}
+    },
+    inactive_sections = {
+        -- these are to remove the defaults
+        lualine_a = {},
+        lualine_v = {},
+        lualine_y = {},
+        lualine_z = {},
+        lualine_c = {},
+        lualine_x = {}
+    },
+    tabline = {},
+    extensions = {"fugitive"}
+}
+
+-- Inserts a component in lualine_c at left section
+local function ins_left(component)
+    table.insert(config.sections.lualine_c, component)
+end
+
+-- Inserts a component in lualine_x ot right section
+local function ins_right(component)
+    table.insert(config.sections.lualine_x, component)
+end
+
+local function ins_left_inactive(component)
+    table.insert(config.inactive_sections.lualine_c, component)
+end
+
+-- Insert left section
+ins_left {
+    "branch",
+    icon = "",
+    color = {fg = colors.violet, bg = "#454545", gui = "bold"}
+}
+
+ins_left {
+    "diff",
+    symbols = {added = " ", modified = "柳", removed = " "},
+    diff_color = {
+        added = {fg = colors.green},
+        modified = {fg = colors.orange},
+        removed = {fg = colors.red}
+    },
+    cond = conditions.hide_in_width
+}
+
+-- Insert mid section.
+ins_left {
+    function()
+        return "%="
+    end
+}
+
+ins_left {
+    "diagnostics",
+    sources = {"nvim_lsp"},
+    symbols = {error = " ", warn = " ", info = " "},
+    diagnostics_color = {
+        color_error = {fg = colors.red},
+        color_warn = {fg = colors.yellow},
+        color_info = {fg = colors.cyan}
+    }
+}
+
+ins_left {
+    "filename",
+    cond = conditions.buffer_not_empty,
+    color = {fg = colors.magenta, gui = "bold"}
+}
+
+-- Inactive mid section
+ins_left_inactive {
+    function()
+        return "%="
+    end
+}
+
+ins_left_inactive {
+    "diagnostics",
+    sources = {"nvim_lsp"},
+    symbols = {error = " ", warn = " ", info = " "},
+    diagnostics_color = {
+        color_error = {fg = colors.red},
+        color_warn = {fg = colors.yellow},
+        color_info = {fg = colors.cyan}
+    }
+}
+
+ins_left_inactive {
+    "filename",
+    cond = conditions.buffer_not_empty,
+    color = {fg = colors.violet}
+}
+
+-- Add components to right sections
+
+ins_right {
+    "o:encoding",
+    fmt = string.lower,
+    cond = conditions.hide_in_width,
+    color = {fg = colors.green},
+    padding = {left = 0, right = 1}
+}
+
+ins_right {
+    "filetype",
+    icons_enabled = true,
+    color = {fg = colors.green},
+    padding = {left = 0, right = 1}
+}
+
+lualine.setup(config)
