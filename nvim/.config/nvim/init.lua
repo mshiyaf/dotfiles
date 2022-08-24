8223cc137c9280107f71bf6754c461d314915719
mshiyaf <mshiyaf@gmail.com>
2022-08-25 00:15:13 +0530

    removed .env.* from global gitignore

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,12 +3,17 @@ require("keymap")
 require("set")
 require("highlights")
 require("maps")
+require("macos")
 require("plugins")
 
 local has = function(x)
-  return vim.fn.has(x) == 1
+	return vim.fn.has(x) == 1
 end
-local is_mac = has "macunix"
+local is_mac = has("macunix")
 if is_mac then
-  require("macos")
+	require("macos")
 end
+
+-- if has("nvim") and vim.fn.executable("nvr") then
+--   -- vim.cmd("let $GIT_EDITOR = 'nvr -cc split --remote-wait +"set bufhidden=wipe''")
+-- end
--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -16,56 +16,57 @@ local silent = { silent = true }
 -- })
 
 -- Reload vim config
-nnoremap('<Leader><CR>', ':so ~/.config/nvim/init.lua<CR>')
+nnoremap("<Leader><CR>", ":so ~/.config/nvim/init.lua<CR>")
 
 -- Don't yank with x
-nnoremap('x', '"_x')
-nnoremap('<leader>d', '"_x')
+nnoremap("x", '"_x')
+nnoremap("<leader>d", '"_x')
 
 -- Increment/decrement
-nnoremap('+', '<C-a>')
-nnoremap('_', '<C-x>')
+nnoremap("+", "<C-a>")
+nnoremap("_", "<C-x>")
 
 -- Select all
-nnoremap('<C-a>', 'gg<S-v>G')
+nnoremap("<C-a>", "gg<S-v>G")
 -- Move a line of text using ALT+[jk] or Command+[jk] on mac
-nnoremap('<A-j>', 'mz:m+<cr>`z')
-nnoremap('<A-k>', 'mz:m-2<cr>`z')
-vnoremap('<A-j>', ":m'>+<cr>`<my`>mzgv`yo`z")
-vnoremap('<A-k>', ":m'<-2<cr>`>my`<mzgv`yo`z")
+nnoremap("<A-j>", "mz:m+<cr>`z")
+nnoremap("<A-k>", "mz:m-2<cr>`z")
+vnoremap("<A-j>", ":m'>+<cr>`<my`>mzgv`yo`z")
+vnoremap("<A-k>", ":m'<-2<cr>`>my`<mzgv`yo`z")
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-nnoremap('te', ':tabedit<CR>', silent)
+nnoremap("te", ":tabedit<CR>", silent)
 
 -- Split window
-nnoremap('ss', ':split<CR><C-w>w', silent)
-nnoremap('sv', ':vsplit<CR><C-w>w', silent)
+nnoremap("ss", ":split<CR><C-w>w", silent)
+nnoremap("sv", ":vsplit<CR><C-w>w", silent)
 
 -- Move window
-nnoremap('<Space>j', '<C-w>w')
-nnoremap('sh', '<C-w>h')
-nnoremap('sk', '<C-w>k')
-nnoremap('sj', '<C-w>j')
-nnoremap('sl', '<C-w>l')
+nnoremap("<Space>j", "<C-w>w")
+nnoremap("sh", "<C-w>h")
+nnoremap("sk", "<C-w>k")
+nnoremap("sj", "<C-w>j")
+nnoremap("sl", "<C-w>l")
 
 -- Resize window
-nnoremap('<C-w><left>', '<C-w><')
-nnoremap('<C-w><right>', '<C-w>>')
-nnoremap('<C-w><up>', '<C-w>+')
-nnoremap('<C-w><down>', '<C-w>-')
-nnoremap('<leader>=', '<C-w>=')
-nnoremap('<leader>m', ':MaximizerToggle<CR>', silent)
-vnoremap('<leader>m', ':MaximizerToggle<CR>gv', silent)
-inoremap('<F3>', '<C-o>:MaximizerToggle<CR>', silent)
+nnoremap("<C-w><left>", "<C-w><")
+nnoremap("<C-w><right>", "<C-w>>")
+nnoremap("<C-w><up>", "<C-w>+")
+nnoremap("<C-w><down>", "<C-w>-")
+nnoremap("<leader>=", "<C-w>=")
+nnoremap("<leader>m", ":MaximizerToggle<CR>", silent)
+vnoremap("<leader>m", ":MaximizerToggle<CR>gv", silent)
+inoremap("<F3>", "<C-o>:MaximizerToggle<CR>", silent)
 
 -- tmux-sessionizer
-nnoremap('<C-f>', ':silent !tmux neww ~/Code/dotfiles/zsh/.config/zsh/tmux-sessionizer.sh<CR>', silent)
+nnoremap("<C-f>", ":silent !tmux neww ~/Code/dotfiles/zsh/.config/zsh/tmux-sessionizer.sh<CR>", silent)
 
 -- terminal
-nmap('<leader>t', ':terminal<Return>i')
+nmap("<leader>t", ":terminal<Return>i")
+nnoremap(";l", ":lua _LAZYGIT_TOGGLE()<cr>")
 
 -- undotree
-nnoremap('<F4>', ':UndotreeToggle<CR>:UndotreeFocus<CR>')
+nnoremap("<F4>", ":UndotreeToggle<CR>:UndotreeFocus<CR>")
