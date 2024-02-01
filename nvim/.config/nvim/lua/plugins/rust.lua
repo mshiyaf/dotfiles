ae5225e0bc60dc3d4adc981febcaee8b99f2d549
mshiyaf <mshiyaf@gmail.com>
2024-02-01 11:48:24 +0530

    feat: added rust tools

---
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^4",
+		ft = { "rust" },
+		dependencies = "neovim/nvim-lspconfig",
+	},
+	{
+		"saecki/crates.nvim",
+		ft = { "toml" },
+		config = function(_, opts)
+			local crates = require("crates")
+			crates.setup(opts)
+			require("cmp").setup.buffer({
+				sources = { { name = "crates" } },
+			})
+			crates.show()
+			require("core.utils").load_mappings("crates")
+		end,
+	},
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+}
