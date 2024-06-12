644052dab4b3d08f98bc888f85876ab1b9383974
Shiyaf <shiyaf@texolworld.com>
2024-06-13 01:36:36 +0530

    feat: add yank highlight autocmd

--- nvim/.config/nvim/lua/mshiyaf/base.lua
+++ nvim/.config/nvim/lua/mshiyaf/base.lua
@@ -6,9 +6,20 @@ vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
 
 -- Add asterisks in block comments
 vim.opt.formatoptions:append({ "r" })
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
