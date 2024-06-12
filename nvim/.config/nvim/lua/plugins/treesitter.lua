9793af8b87608cdfb10e2e705d317bbc06d31db5
Shiyaf <shiyaf@texolworld.com>
2024-06-12 20:36:21 +0530

    feat: fix treesitter issues

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -37,9 +37,9 @@ return {
           "markdown",
           "markdown_inline",
         },
-        autotag = {
-          enable = true,
-        },
+        -- autotag = {
+        --   enable = true,
+        -- },
       })
 
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
--- nvim/.config/nvim/lua/plugins/utils.lua
+++ nvim/.config/nvim/lua/plugins/utils.lua
@@ -1,37 +1,40 @@
 return {
-	{
-		"windwp/nvim-ts-autotag",
-		opts = {},
-	},
-	{ "szw/vim-maximizer" },
-	{ "mong8se/actually.nvim" },
-	{ "nvim-tree/nvim-web-devicons" },
-	{ "RRethy/vim-illuminate" },
-	{ "jwalton512/vim-blade" },
-	{ "nvim-telescope/telescope-ui-select.nvim" },
-	{ "PatschD/zippy.nvim" },
-	{
-		"numToStr/Comment.nvim",
-		opts = {
-			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-		},
-		lazy = false,
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-	},
-	{
-		"j-hui/fidget.nvim",
-		tag = "legacy",
-		opts = {},
-	},
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end
+  },
+  { "szw/vim-maximizer" },
+  { "mong8se/actually.nvim" },
+  { "nvim-tree/nvim-web-devicons" },
+  { "RRethy/vim-illuminate" },
+  { "jwalton512/vim-blade" },
+  { "nvim-telescope/telescope-ui-select.nvim" },
+  { "PatschD/zippy.nvim" },
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    },
+    lazy = false,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+  },
+  {
+    "j-hui/fidget.nvim",
+    tag = "legacy",
+    opts = {},
+  },
 }
