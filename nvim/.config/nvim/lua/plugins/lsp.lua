b7dd1af4a014a7e2f26527c8f0ac31be1ac7bd0a
Shiyaf <shiyaf@texolworld.com>
2024-06-14 12:04:03 +0530

    feat: change to i3 ws jump

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,127 +1,141 @@
 return {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-        -- LSP Support
-        { "neovim/nvim-lspconfig" },
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" },
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v2.x",
+  dependencies = {
+    -- LSP Support
+    { "neovim/nvim-lspconfig" },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 
-        -- Autocompletion
-        { "hrsh7th/nvim-cmp" },
-        { "hrsh7th/cmp-buffer" },
-        { "hrsh7th/cmp-path" },
-        { "hrsh7th/cmp-cmdline" },
-        { "saadparwaiz1/cmp_luasnip" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/cmp-nvim-lua" },
+    -- Autocompletion
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
+    { "saadparwaiz1/cmp_luasnip" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-nvim-lua" },
 
-        -- Snippets
-        { "L3MON4D3/LuaSnip" },
-        { "rafamadriz/friendly-snippets" },
-    },
-    config = function()
-        local lsp = require("lsp-zero")
-        local cmp = require("cmp")
+    -- Snippets
+    { "L3MON4D3/LuaSnip" },
+    { "rafamadriz/friendly-snippets" },
+  },
+  config = function()
+    local lsp = require("lsp-zero")
+    local cmp = require("cmp")
 
-        lsp.preset("recommended")
+    lsp.preset("recommended")
 
-        lsp.ensure_installed({
-            "tsserver",
-            "rust_analyzer",
-            "tailwindcss",
-            "eslint",
-            "gopls",
-            "lua_ls",
-        })
+    lsp.ensure_installed({
+      "tsserver",
+      "black",
+      "delve",
+      "eslint-lsp eslint",
+      "rust_analyzer",
+      "tailwindcss",
+      "eslint",
+      "gopls",
+      "goimports",
+      "lua_ls",
+      "isort",
+      "lua-language-server",
+      "prettier",
+      "prettierd",
+      "rust-analyzer",
+      "sql-formatter",
+      "tailwindcss-language-server",
+      "typescript-language-server"
+    })
 
-        lsp.configure("lua_ls", {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-        local cmp_mappings = lsp.defaults.cmp_mappings({
-            ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-            ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        })
 
-        cmp_mappings["<Tab>"] = nil
-        cmp_mappings["<S-Tab>"] = nil
+    lsp.configure("lua_ls", {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+        },
+      },
+    })
 
-        lsp.setup_nvim_cmp({
-            mapping = cmp_mappings,
-        })
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+    local cmp_mappings = lsp.defaults.cmp_mappings({
+      ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+      ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    })
 
-        lsp.set_preferences({
-            suggest_lsp_servers = false,
-            sign_icons = {
-                error = "E",
-                warn = "W",
-                hint = "H",
-                info = "I",
-            },
-        })
+    cmp_mappings["<Tab>"] = nil
+    cmp_mappings["<S-Tab>"] = nil
 
-        lsp.on_attach(function(client, bufnr)
-            if client.textDocument then
-                client.textDocument.foldingRange = {
-                    dynamicRegistration = false,
-                    lineFoldingOnly = true,
-                }
-            end
-            local opts = { buffer = bufnr, remap = false }
+    lsp.setup_nvim_cmp({
+      mapping = cmp_mappings,
+    })
 
-            vim.keymap.set("n", "gd", function()
-                vim.lsp.buf.definition()
-            end, opts)
-            vim.keymap.set("n", "gi", function()
-                vim.lsp.buf.implementation()
-            end, opts)
-            vim.keymap.set("n", "<leader>vws", function()
-                vim.lsp.buf.workspace_symbol()
-            end, opts)
-            vim.keymap.set("n", "<leader>vd", function()
-                vim.diagnostic.open_float()
-            end, opts)
-            vim.keymap.set("n", "d]", function()
-                vim.diagnostic.goto_next()
-            end, opts)
-            vim.keymap.set("n", "d[", function()
-                vim.diagnostic.goto_prev()
-            end, opts)
-            vim.keymap.set("n", "<leader>ca", function()
-                vim.lsp.buf.code_action()
-            end, opts)
-            vim.keymap.set("n", "gr", function()
-                vim.lsp.buf.references()
-            end, opts)
-            vim.keymap.set("n", "<leader>vrn", function()
-                vim.lsp.buf.rename()
-            end, opts)
-            vim.keymap.set("n", "<C-h>", function()
-                vim.lsp.buf.signature_help()
-            end, opts)
-        end)
+    lsp.set_preferences({
+      suggest_lsp_servers = false,
+      sign_icons = {
+        error = "E",
+        warn = "W",
+        hint = "H",
+        info = "I",
+      },
+    })
 
-        lsp.set_sign_icons({
-            error = '✘',
-            warn = '▲',
-            hint = '⚑',
-            info = '»'
-        })
+    lsp.on_attach(function(client, bufnr)
+      if client.textDocument then
+        client.textDocument.foldingRange = {
+          dynamicRegistration = false,
+          lineFoldingOnly = true,
+        }
+      end
+      local opts = { buffer = bufnr, remap = false }
 
-        lsp.setup()
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+      end, opts)
+      vim.keymap.set("n", "gi", function()
+        vim.lsp.buf.implementation()
+      end, opts)
+      vim.keymap.set("n", "<leader>vws", function()
+        vim.lsp.buf.workspace_symbol()
+      end, opts)
+      vim.keymap.set("n", "<leader>vd", function()
+        vim.diagnostic.open_float()
+      end, opts)
+      vim.keymap.set("n", "d]", function()
+        vim.diagnostic.goto_next()
+      end, opts)
+      vim.keymap.set("n", "d[", function()
+        vim.diagnostic.goto_prev()
+      end, opts)
+      vim.keymap.set("n", "<leader>ca", function()
+        vim.lsp.buf.code_action()
+      end, opts)
+      vim.keymap.set("n", "gr", function()
+        vim.lsp.buf.references()
+      end, opts)
+      vim.keymap.set("n", "<leader>vrn", function()
+        vim.lsp.buf.rename()
+      end, opts)
+      vim.keymap.set("n", "<C-h>", function()
+        vim.lsp.buf.signature_help()
+      end, opts)
+    end)
 
-        vim.diagnostic.config({
-            virtual_text = true,
-        })
-    end,
+    lsp.set_sign_icons({
+      error = '✘',
+      warn = '▲',
+      hint = '⚑',
+      info = '»'
+    })
+
+    lsp.setup()
+
+    vim.diagnostic.config({
+      virtual_text = true,
+    })
+  end,
 }
