0cbcf033d2f0904e5ed688aeb92c73c3c58a69a7
shiyaf <shiyaf@texolworld.com>
2024-07-20 04:54:28 +0530

    feat: add all lazy vim opts and extras

--- lvim/.config/lvim/lua/config/keymaps.lua
+++ lvim/.config/lvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- silent
+local silent = { silent = true }
+
+-- tmux-sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/code/dotfiles/zsh/.config/zsh/tmux-sessionizer.sh<CR>", silent)
