9c08166a4c184dbd6e9bd16805d03445865d3bd3
Shiyaf <shiyaf@texolworld.com>
2025-08-20 13:09:47 +0530

    temp fix for lvim

--- lvim/.config/lvim/lua/plugins/colorscheme.lua
+++ lvim/.config/lvim/lua/plugins/colorscheme.lua
@@ -30,6 +30,16 @@ return {
             },
         },
     },
+    -- TODO: Remove after fix from folke
+    {
+        "akinsho/bufferline.nvim",
+        init = function()
+            local bufline = require("catppuccin.groups.integrations.bufferline")
+            function bufline.get()
+                return bufline.get_theme()
+            end
+        end,
+    },
     -- Configure LazyVim to load the colorscheme
     {
         "LazyVim/LazyVim",
