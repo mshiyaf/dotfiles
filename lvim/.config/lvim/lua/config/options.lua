1512ad3d53db0d06d89d24ec7cb7a06cc3604c8c
mshiyaf <mshiyaf@gmail.com>
2026-01-08 23:50:21 +0530

    updates to neovide config

--- lvim/.config/lvim/lua/config/options.lua
+++ lvim/.config/lvim/lua/config/options.lua
@@ -5,3 +5,19 @@
 -- View a color column at 80 characters
 vim.opt.colorcolumn = "80"
 vim.g.lazyvim_php_lsp = "intelephense"
+
+
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font:h10"
+    vim.g.neovide_padding_top = 0
+    vim.g.neovide_padding_bottom = 0
+    vim.g.neovide_padding_right = 0
+    vim.g.neovide_padding_left = 0
+    -- vim.g.neovide_floating_blur_amount_x = 2.0
+    -- vim.g.neovide_floating_blur_amount_y = 2.0
+    -- vim.g.neovide_transparency = 0.9
+    if vim.fn.argc() == 0 then
+      vim.cmd("cd C:/Users/shiya/dev")
+    end
+end
+
