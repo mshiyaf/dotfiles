1caebc62237603dbef0676aaa7279403080beaa6
Shiyaf <shiyaf@texolworld.com>
2026-02-26 14:42:25 +0530

    update themes

--- lvim/.config/lvim/lua/matugen.lua
+++ lvim/.config/lvim/lua/matugen.lua
@@ -3,24 +3,24 @@ local M = {}
 function M.setup()
     require("base16-colorscheme").setup({
         -- Background tones
-        base00 = "#1a1b26", -- Default Background
-        base01 = "#24283b", -- Lighter Background (status bars)
-        base02 = "#2c3148", -- Selection Background
-        base03 = "#586691", -- Comments, Invisibles
+        base00 = "#0b0e14", -- Default Background
+        base01 = "#1e222a", -- Lighter Background (status bars)
+        base02 = "#262c36", -- Selection Background
+        base03 = "#595f6a", -- Comments, Invisibles
         -- Foreground tones
-        base04 = "#9aa5ce", -- Dark Foreground (status bars)
-        base05 = "#c0caf5", -- Default Foreground
-        base06 = "#c0caf5", -- Light Foreground
-        base07 = "#c0caf5", -- Lightest Foreground
+        base04 = "#636a72", -- Dark Foreground (status bars)
+        base05 = "#bfbdb6", -- Default Foreground
+        base06 = "#bfbdb6", -- Light Foreground
+        base07 = "#bfbdb6", -- Lightest Foreground
         -- Accent colors
-        base08 = "#f7768e", -- Variables, XML Tags, Errors
-        base09 = "#9ece6a", -- Integers, Constants
-        base0A = "#bb9af7", -- Classes, Search Background
-        base0B = "#7aa2f7", -- Strings, Diff Inserted
-        base0C = "#c1e996", -- Regex, Escape Chars
-        base0D = "#87abf8", -- Functions, Methods
-        base0E = "#af89f6", -- Keywords, Storage
-        base0F = "#bb0023", -- Deprecated, Embedded Tags
+        base08 = "#d95757", -- Variables, XML Tags, Errors
+        base09 = "#39bae6", -- Integers, Constants
+        base0A = "#aad94c", -- Classes, Search Background
+        base0B = "#e6b450", -- Strings, Diff Inserted
+        base0C = "#8ed8f1", -- Regex, Escape Chars
+        base0D = "#efcf8f", -- Functions, Methods
+        base0E = "#cde996", -- Keywords, Storage
+        base0F = "#700e0e", -- Deprecated, Embedded Tags
     })
 end
 
